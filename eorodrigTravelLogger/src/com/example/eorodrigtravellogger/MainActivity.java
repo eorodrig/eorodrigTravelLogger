7bcf47dd124200d5de5822b4b5f61161ee2d7496
eorodrig <eorodrig@ualberta.ca>
2015-01-12 22:49:44 -0700

    Started Design of Application 	Added view/edit claim

--- eorodrigTravelLogger/src/com/example/eorodrigtravellogger/MainActivity.java
+++ eorodrigTravelLogger/src/com/example/eorodrigtravellogger/MainActivity.java
@@ -4,7 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends Activity {
 
@@ -33,4 +34,11 @@ public class MainActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+    
+    public void onClickViewEditClaim(View view){
+    	 Intent intent = new Intent(this, ViewEditClaim.class);
+    	 startActivity(intent);
+    }
+    
+    
 }
