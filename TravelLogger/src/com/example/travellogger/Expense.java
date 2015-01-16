47c0904a94908fc18e8e699d9c3a64495e428f13
eorodrig <eorodrig@ualberta.ca>
2015-01-16 01:35:55 -0700

    worked on making Add Claim functional worked on Automatically adding new claims to the claim list activity

---
+++ TravelLogger/src/com/example/travellogger/Expense.java
@@ -0,0 +1,50 @@
+package com.example.travellogger;
+
+public class Expense {
+
+	protected String date;
+	
+	protected String category;
+	protected String description;
+	
+	protected double amountSpent;
+	protected String currency;
+	
+	
+	/**
+	 * This is a constructor for an expense item. Every expense needs the following information.
+	 * @param expenseDate	= Date expense occurred
+	 * @param category		= Category for expense
+	 * @param description	= Short description of expense
+	 * @param amountSpent	= Expense Amount
+	 * @param currency		= Currency Type
+	 */
+	public Expense(String expenseDate, String category, String description, double amountSpent, String currency){
+		this.date = expenseDate;
+		this.category = category;
+		this.description = description;
+		this.amountSpent = amountSpent;
+		this.currency = currency;
+	}
+	
+	
+	
+	/**
+	 * This method updates the expense information based on the user information
+	 * @param expenseDate	= Date expense occurred
+	 * @param category		= Category for expense
+	 * @param description	= Short description of expense
+	 * @param amountSpent	= Expense Amount
+	 * @param currency		= Currency Type
+	 */
+	public void updateExpense(String expenseDate, String category, String description, double amountSpent, String currency){
+		this.date = expenseDate;
+		this.category = category;
+		this.description = description;
+		this.amountSpent = amountSpent;
+		this.currency = currency;
+	}
+	
+	
+	
+}
