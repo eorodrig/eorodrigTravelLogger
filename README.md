b8b81abaa21d1c928deb18474a6d2ace9fc504f4
eorodrig <eorodrig@ualberta.ca>
2015-02-01 03:02:26 -0700

    added save/load feature modified start menu and claim list menu

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ www.compiletimeerror.com/2013/01/how-to-set-background-image-in-android.html#.VL
 
 http://stackoverflow.com/questions/5799140/java-get-month-string-from-integer  --Accessed 2015 jan 14 @10:55 	date formatting
 http://stackoverflow.com/questions/17674308/date-from-edittext--Accessed 2015 jan 14 @11:45 	date formatting
+
+
+
+http://stackoverflow.com/questions/5813434/trouble-with-gson-serializing-an-arraylist-of-pojos  jan 31/2015 @ 1050pm  gson
---
+++ TravelLogger/src/com/eorodrig/TravelLogger/Expense.java
@@ -0,0 +1,93 @@
+package com.eorodrig.TravelLogger;
+
+import java.util.Date;
+
+import android.text.format.DateFormat;
+
+public class Expense {
+
+	protected Date date;
+	protected String expenseDate;
+	
+	protected String category;
+	protected String description;
+	
+	protected double amountSpent;
+	protected String currency;
+	
+	
+	
+	protected int currencySpinnerID, categorySpinnerID;
+	
+	
+	/**
+	 * This is a constructor for an expense item. Every expense needs the following information.
+	 * @param expenseDate	= Date expense occurred
+	 * @param category		= Category for expense
+	 * @param description	= Short description of expense
+	 * @param amountSpent	= Expense Amount
+	 * @param currency		= Currency Type
+	 */
+	public Expense(Date expenseDate, String category, String description, double amountSpent, String currency, int currencyID, int categoryID){
+		this.date = expenseDate;
+		this.category = category;
+		this.description = description;
+		this.amountSpent = amountSpent;
+		this.currency = currency;
+		this.categorySpinnerID = categoryID;
+		this.currencySpinnerID = currencyID;
+	}
+	
+	
+	
+	/**
+	 * This method updates the expense information based on the user information
+	 * @param expenseDate	= Date expense occurred
+	 * @param category		= Category for expense
+	 * @param description	= Short description of expense
+	 * @param amountSpent	= Expense Amount
+	 * @param currency		= Currency Type
+	 */
+	public void updateExpense(Date expenseDate, String category, String description, double amountSpent, String currency, int currencyID, int categoryID){
+		this.date = expenseDate;
+		this.category = category;
+		this.description = description;
+		this.amountSpent = amountSpent;
+		this.currency = currency;
+		this.categorySpinnerID = categoryID;
+		this.currencySpinnerID = currencyID;
+	}
+	
+	
+	public String toString(){
+		
+		DateFormat dateFormat = new DateFormat();
+		
+		this.expenseDate = dateFormat.format("dd-MMM-yyyy", this.date).toString();
+		
+		return this.category + "\n" + this.description + "\n" + this.expenseDate +  "\n" + "Total: " + this.amountSpent  + " " +  this.currency.substring(0, 3)  ; 
+
+		
+	}
+	
+	public Date getDate(){
+		return date;
+	}
+	
+	public int getCategoryID(){
+		return categorySpinnerID;
+	}
+	
+	public String getDescription(){
+		return description;
+	}
+	
+	public String getAmount(){
+		return String.valueOf(amountSpent);
+	}
+	
+	public int getCurrencyID(){
+		return currencySpinnerID;
+	}
+	
+}
